36bfa47e756e014242925df7ce2e324a19fbc82e
Twaza-j <atwazagye@gmail.com>
2023-10-31 03:42:29 -0700

    prints all letters excep q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for alphbt in range(97, 123):
-    if chr(alphbt) not in ('e', 'q'):
-        print(chr(alphbt), end="")
+    if chr(alphbt) != 'q' and chr(alphbt) != 'e':
+        print("{}".format(chr(alphbt)), end="")
         
